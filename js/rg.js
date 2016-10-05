1a1d63afdc9c52c5d1623e0002ecea936ad1b659
Miller Nguyen <miller_nguyen@yahoo.com>
2016-10-05 11:34:02 +0800

    fixed issue of selected row by checking objects

--- js/rg.js
+++ js/rg.js
@@ -1,11 +1,11 @@
-riot.tag2('rg', '<div class="riotGrid"> <div class="riotGridHeader {topPanelCss}"> <span if="{allowSearch==true}"> <input type="text" id="txtSearch" placeholder="enter text to search"> <button type="button" onclick="{btnSearchClicked}"><i class="glyphicon glyphicon-search"></i>&nbsp;{searchBtnText}</button> <button type="button" onclick="{btnClearSearchClicked}"><i class="glyphicon glyphicon-remove"></i>&nbsp;{clearSearchBtnText}</button> </span> <button type="button" if="{allowAddNew==true}" onclick="{btnAddNewClicked}">{addNewBtnText}</button> <button type="button" if="{allowExport==true}" onclick="{btnExportClicked}">{exportBtnText}</button> <button type="button" onclick="{btnShowFilters}" if="{allowFilters==true}"> <i class="glyphicon glyphicon-filter"></i>&nbsp;{showFilters==true? hideFilterBtnText: showFilterBtnText} </button> <span style="float: right">{totalLblText}&nbsp;{formatNumber(data.length)} {recLblText}</span> </div> <div class="riotGridBody"> <div> <div class="riotGridBodyDetail"> <table class="rg-table tableBody"> <thead> <tr> <th each="{cols}" riot-style="width:{width? width:\'50px\'};" class="{this.headerCss}" data-field="{id}"><a data-field="{id}" onclick="{sortData}">{title}</a></th> </tr> <tr show="{showFilters}"> <th each="{cols}" riot-style="width:{width? width:\'50px\'};" class="{this.headerCss}" data-field="{id}"><input type="text" class="input-filter" onkeyup="{filterData}" data-field="{id}"></th> </tr> </thead> <tbody> <tr data-is="rg-cells" each="{e, index in gridData}" class="{e.css} {rg-selectedRow: Item[key] == e[key], rg-altRow: (index % 2 == 1)}" ondblclick="{rowDbClick}" onclick="{rowClicked}"></tr> </tbody> </table> </div> </div> </div> <div class="riotGridFooter {bottomPanelCss}"> <div class="btnLeft {topPanelBkgColor}"> <div class="btnGroup" show="{Item}"> <button each="{buttonLefts}" type="button" class="{css}" data-field="{value}" onclick="{btnClicked}" name="{name}">{btnName}</button> </div> </div> <div class="paging"> <button type="button" onclick="{btnFirstRecordClicked}" __disabled="{!(selectedPage>0)}"> <strong>{firstBtnText}</strong> </button> <button type="button" onclick="{btnPreRecordClicked}" __disabled="{!(selectedPage>0)}"> <strong>{prevBtnText}</strong> </button> <select id="cboPages" onchange="{cboPagesChanged}"> <option value="{pageNo}" each="{pages}" __selected="{selectedPage==pageNo}">{pageNo+1}</option> </select> <span>/{pages.length}</span> <button type="button" onclick="{btnNextRecordClicked}" __disabled="{!(selectedPage<pages.length-1)}"> <strong>{nextBtnText}</strong> </button> <button type="button" onclick="{btnLastRecordClicked}" __disabled="{!(selectedPage<pages.length-1)}"> <strong>{lastBtnText}</strong> </button> </div> <div class="btnRight"> <div class="btnGroup" show="{Item}"> <button each="{buttonRights}" type="button" class="{css}" data-field="{value}" onclick="{btnClicked}" name="{name}">{btnName}</button> </div> </div> </div> </div>', 'rg .rg-show-more,[riot-tag="rg"] .rg-show-more,[data-is="rg"] .rg-show-more{ overflow-y: auto !important; overflow-x: hidden; max-height: 200px; min-height: 30px; white-space: normal !important; display: block; } rg a,[riot-tag="rg"] a,[data-is="rg"] a{ cursor: pointer; } rg .paging,[riot-tag="rg"] .paging,[data-is="rg"] .paging{ text-align: center; align-content: center; display: table-cell; width: 33.33%; } rg .btnRight,[riot-tag="rg"] .btnRight,[data-is="rg"] .btnRight{ display: table-cell; align-content: flex-end; width: 33.33%; text-align: right; } rg .btnLeft,[riot-tag="rg"] .btnLeft,[data-is="rg"] .btnLeft{ display: table-cell; align-content: flex-start; width: 33.33%; } rg .rg-left,[riot-tag="rg"] .rg-left,[data-is="rg"] .rg-left{ text-align: left; } rg .rg-right,[riot-tag="rg"] .rg-right,[data-is="rg"] .rg-right{ text-align: right; } rg .rg-center,[riot-tag="rg"] .rg-center,[data-is="rg"] .rg-center{ text-align: center; } rg .riotGridHeader,[riot-tag="rg"] .riotGridHeader,[data-is="rg"] .riotGridHeader,rg .riotGridBody,[riot-tag="rg"] .riotGridBody,[data-is="rg"] .riotGridBody{ display: block; width: 100%; padding: 3px; border: 1px solid #808080; } rg .riotGridBody,[riot-tag="rg"] .riotGridBody,[data-is="rg"] .riotGridBody{ max-width: 100%; overflow: auto; } rg .riotGridFooter,[riot-tag="rg"] .riotGridFooter,[data-is="rg"] .riotGridFooter{ background-color: #bed9db; } rg .riotGridHeader,[riot-tag="rg"] .riotGridHeader,[data-is="rg"] .riotGridHeader{ background-color: #eafbfc; } rg .riotGridFooter,[riot-tag="rg"] .riotGridFooter,[data-is="rg"] .riotGridFooter{ display: flex; width: 100%; padding: 3px; border: 1px solid #808080; background-color: #bed9db; } rg .riotGridBodyDetail,[riot-tag="rg"] .riotGridBodyDetail,[data-is="rg"] .riotGridBodyDetail{ max-width: 100%; overflow-y: auto; height: 500px; margin: 0px; } rg ::-webkit-scrollbar,[riot-tag="rg"] ::-webkit-scrollbar,[data-is="rg"] ::-webkit-scrollbar{ width: 0.5em; height: 0.5em; } rg ::-webkit-scrollbar-track,[riot-tag="rg"] ::-webkit-scrollbar-track,[data-is="rg"] ::-webkit-scrollbar-track{ -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3); } rg ::-webkit-scrollbar-thumb,[riot-tag="rg"] ::-webkit-scrollbar-thumb,[data-is="rg"] ::-webkit-scrollbar-thumb{ background-color: maroon; outline: 1px solid red; } rg .riotGridFixHeader,[riot-tag="rg"] .riotGridFixHeader,[data-is="rg"] .riotGridFixHeader{ max-width: 100%; overflow: hidden; margin: 0px; padding-right: 5px; } rg .rg-table tbody,[riot-tag="rg"] .rg-table tbody,[data-is="rg"] .rg-table tbody{ overflow-y: scroll; } rg .rg-table,[riot-tag="rg"] .rg-table,[data-is="rg"] .rg-table{ border-spacing: 0; table-layout: fixed; } rg .rg-table td,[riot-tag="rg"] .rg-table td,[data-is="rg"] .rg-table td,rg th,[riot-tag="rg"] th,[data-is="rg"] th{ border: 1px solid #eee; padding: 3px; overflow: hidden; white-space: nowrap; } rg .rg-table th,[riot-tag="rg"] .rg-table th,[data-is="rg"] .rg-table th{ text-align: center; background-color: black; color: yellow !important; } rg .rg-table th a,[riot-tag="rg"] .rg-table th a,[data-is="rg"] .rg-table th a{ color: yellow !important; } rg .rg-table th.resizing,[riot-tag="rg"] .rg-table th.resizing,[data-is="rg"] .rg-table th.resizing{ cursor: col-resize; } rg .rg-table th input,[riot-tag="rg"] .rg-table th input,[data-is="rg"] .rg-table th input{ width: 100%; max-width: 100%; background-color: lightyellow; border: 0.01em solid #808080; color: black; font-weight: normal; } rg .selectedRow,[riot-tag="rg"] .selectedRow,[data-is="rg"] .selectedRow{ background-color: yellow !important; } rg .rg-checkbox,[riot-tag="rg"] .rg-checkbox,[data-is="rg"] .rg-checkbox{ cursor: pointer; width: 16px; height: 16px; display: block; background-repeat: no-repeat; margin: 0 auto; }', '', function(opts) {
+riot.tag2('rg', '<div class="riotGrid"> <div class="riotGridHeader {topPanelCss}"> <span if="{allowSearch==true}"> <input type="text" id="txtSearch" placeholder="enter text to search"> <button type="button" onclick="{btnSearchClicked}"><i class="glyphicon glyphicon-search"></i>&nbsp;{searchBtnText}</button> <button type="button" onclick="{btnClearSearchClicked}"><i class="glyphicon glyphicon-remove"></i>&nbsp;{clearSearchBtnText}</button> </span> <button type="button" if="{allowAddNew==true}" onclick="{btnAddNewClicked}">{addNewBtnText}</button> <button type="button" if="{allowExport==true}" onclick="{btnExportClicked}">{exportBtnText}</button> <button type="button" onclick="{btnShowFilters}" if="{allowFilters==true}"> <i class="glyphicon glyphicon-filter"></i>&nbsp;{showFilters==true? hideFilterBtnText: showFilterBtnText} </button> <span style="float: right">{totalLblText}&nbsp;{formatNumber(data.length)} {recLblText}</span> </div> <div class="riotGridBody"> <div> <div class="riotGridBodyDetail"> <table class="rg-table tableBody"> <thead> <tr> <th each="{cols}" riot-style="width:{width? width:\'50px\'};" class="{this.headerCss}" data-field="{id}"><a data-field="{id}" onclick="{sortData}">{title}</a></th> </tr> <tr show="{showFilters}"> <th each="{cols}" riot-style="width:{width? width:\'50px\'};" class="{this.headerCss}" data-field="{id}"><input type="text" class="input-filter" onkeyup="{filterData}" data-field="{id}"></th> </tr> </thead> <tbody> <tr data-is="rg-cells" each="{e, index in gridData}" class="{e.css} {rg-selectedRow: JSON.stringify(rgSelectedRow) === JSON.stringify(e), rg-altRow: (index % 2 == 1)}" ondblclick="{rowDbClick}" onclick="{rowClicked}"></tr> </tbody> </table> </div> </div> </div> <div class="riotGridFooter {bottomPanelCss}"> <div class="btnLeft {topPanelBkgColor}"> <div class="btnGroup" show="{rgSelectedRow}"> <button each="{buttonLefts}" type="button" class="{css}" data-field="{value}" onclick="{btnClicked}" name="{name}">{btnName}</button> </div> </div> <div class="paging"> <button type="button" onclick="{btnFirstRecordClicked}" __disabled="{!(selectedPage>0)}"> <strong>{firstBtnText}</strong> </button> <button type="button" onclick="{btnPreRecordClicked}" __disabled="{!(selectedPage>0)}"> <strong>{prevBtnText}</strong> </button> <select id="cboPages" onchange="{cboPagesChanged}"> <option value="{pageNo}" each="{pages}" __selected="{selectedPage==pageNo}">{pageNo+1}</option> </select> <span>/{pages.length}</span> <button type="button" onclick="{btnNextRecordClicked}" __disabled="{!(selectedPage<pages.length-1)}"> <strong>{nextBtnText}</strong> </button> <button type="button" onclick="{btnLastRecordClicked}" __disabled="{!(selectedPage<pages.length-1)}"> <strong>{lastBtnText}</strong> </button> </div> <div class="btnRight"> <div class="btnGroup" show="{rgSelectedRow}"> <button each="{buttonRights}" type="button" class="{css}" data-field="{value}" onclick="{btnClicked}" name="{name}">{btnName}</button> </div> </div> </div> </div>', 'rg .rg-show-more,[riot-tag="rg"] .rg-show-more,[data-is="rg"] .rg-show-more{ overflow-y: auto !important; overflow-x: hidden; max-height: 200px; min-height: 30px; white-space: normal !important; display: block; } rg a,[riot-tag="rg"] a,[data-is="rg"] a{ cursor: pointer; } rg .paging,[riot-tag="rg"] .paging,[data-is="rg"] .paging{ text-align: center; align-content: center; display: table-cell; width: 33.33%; } rg .btnRight,[riot-tag="rg"] .btnRight,[data-is="rg"] .btnRight{ display: table-cell; align-content: flex-end; width: 33.33%; text-align: right; } rg .btnLeft,[riot-tag="rg"] .btnLeft,[data-is="rg"] .btnLeft{ display: table-cell; align-content: flex-start; width: 33.33%; } rg .rg-left,[riot-tag="rg"] .rg-left,[data-is="rg"] .rg-left{ text-align: left; } rg .rg-right,[riot-tag="rg"] .rg-right,[data-is="rg"] .rg-right{ text-align: right; } rg .rg-center,[riot-tag="rg"] .rg-center,[data-is="rg"] .rg-center{ text-align: center; } rg .riotGridHeader,[riot-tag="rg"] .riotGridHeader,[data-is="rg"] .riotGridHeader,rg .riotGridBody,[riot-tag="rg"] .riotGridBody,[data-is="rg"] .riotGridBody{ display: block; width: 100%; padding: 3px; border: 1px solid #808080; } rg .riotGridBody,[riot-tag="rg"] .riotGridBody,[data-is="rg"] .riotGridBody{ max-width: 100%; overflow: auto; } rg .riotGridFooter,[riot-tag="rg"] .riotGridFooter,[data-is="rg"] .riotGridFooter{ background-color: #bed9db; } rg .riotGridHeader,[riot-tag="rg"] .riotGridHeader,[data-is="rg"] .riotGridHeader{ background-color: #eafbfc; } rg .riotGridFooter,[riot-tag="rg"] .riotGridFooter,[data-is="rg"] .riotGridFooter{ display: flex; width: 100%; padding: 3px; border: 1px solid #808080; background-color: #bed9db; } rg .riotGridBodyDetail,[riot-tag="rg"] .riotGridBodyDetail,[data-is="rg"] .riotGridBodyDetail{ max-width: 100%; overflow-y: auto; height: 500px; margin: 0px; } rg ::-webkit-scrollbar,[riot-tag="rg"] ::-webkit-scrollbar,[data-is="rg"] ::-webkit-scrollbar{ width: 0.5em; height: 0.5em; } rg ::-webkit-scrollbar-track,[riot-tag="rg"] ::-webkit-scrollbar-track,[data-is="rg"] ::-webkit-scrollbar-track{ -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3); } rg ::-webkit-scrollbar-thumb,[riot-tag="rg"] ::-webkit-scrollbar-thumb,[data-is="rg"] ::-webkit-scrollbar-thumb{ background-color: maroon; outline: 1px solid red; } rg .riotGridFixHeader,[riot-tag="rg"] .riotGridFixHeader,[data-is="rg"] .riotGridFixHeader{ max-width: 100%; overflow: hidden; margin: 0px; padding-right: 5px; } rg .rg-table tbody,[riot-tag="rg"] .rg-table tbody,[data-is="rg"] .rg-table tbody{ overflow-y: scroll; } rg .rg-table,[riot-tag="rg"] .rg-table,[data-is="rg"] .rg-table{ border-spacing: 0; table-layout: fixed; } rg .rg-table td,[riot-tag="rg"] .rg-table td,[data-is="rg"] .rg-table td,rg th,[riot-tag="rg"] th,[data-is="rg"] th{ border: 1px solid #eee; padding: 3px; overflow: hidden; white-space: nowrap; } rg .rg-table th,[riot-tag="rg"] .rg-table th,[data-is="rg"] .rg-table th{ text-align: center; background-color: black; color: yellow !important; } rg .rg-table th a,[riot-tag="rg"] .rg-table th a,[data-is="rg"] .rg-table th a{ color: yellow !important; } rg .rg-table th.resizing,[riot-tag="rg"] .rg-table th.resizing,[data-is="rg"] .rg-table th.resizing{ cursor: col-resize; } rg .rg-table th input,[riot-tag="rg"] .rg-table th input,[data-is="rg"] .rg-table th input{ width: 100%; max-width: 100%; background-color: lightyellow; border: 0.01em solid #808080; color: black; font-weight: normal; } rg .selectedRow,[riot-tag="rg"] .selectedRow,[data-is="rg"] .selectedRow{ background-color: yellow !important; } rg .rg-checkbox,[riot-tag="rg"] .rg-checkbox,[data-is="rg"] .rg-checkbox{ cursor: pointer; width: 16px; height: 16px; display: block; background-repeat: no-repeat; margin: 0 auto; }', '', function(opts) {
         this.cols = [];
         this.data = [];
         this.rawData = [];
         this.gridData = [];
         this.pages = [];
         this.sorted = { field: 'ID', sort: 'desc' };
-        this.Item = null;
+        this.rgSelectedRow = null;
         this.showFilters = false;
         this.rowNoWidth = 30;
         this.buttonLefts = [];
@@ -28,7 +28,7 @@ riot.tag2('rg', '<div class="riotGrid"> <div class="riotGridHeader {topPanelCss}
         this.press = false;
         this.moving = false;
         this.key = 'ID';
-        this.allowResizeCol = false;
+        this.allowResizeCol = true;
 
         this.addNewBtnText = 'Add New';
         this.showFilterBtnText = 'Show Filters';
@@ -53,6 +53,7 @@ riot.tag2('rg', '<div class="riotGrid"> <div class="riotGridHeader {topPanelCss}
         var self = this;
         var startX, startWidth, col;
         var self = this;
+
         this.loadData = function (data, options) {
 
             var obj = Object.assign({}, options);
@@ -122,6 +123,7 @@ riot.tag2('rg', '<div class="riotGrid"> <div class="riotGridHeader {topPanelCss}
                 self.mouseEventInit();
             }
         }
+
         this.btnAddNewClicked = function () {
             self.trigger('add', null);
         }
@@ -196,11 +198,11 @@ riot.tag2('rg', '<div class="riotGrid"> <div class="riotGridHeader {topPanelCss}
         }
 
         this.rowClicked = function (e) {
-            self.Item = e.item.e;
-            self.trigger('rowClicked', self.Item);
+            self.rgSelectedRow = e.item.e;
+            self.trigger('rowClicked', self.rgSelectedRow);
             var obj = e.target;
             var cell = {
-                item: self.Item,
+                item: self.rgSelectedRow,
                 cell: obj
             }
             self.trigger('cellClicked', cell);
@@ -211,17 +213,17 @@ riot.tag2('rg', '<div class="riotGrid"> <div class="riotGridHeader {topPanelCss}
         }
 
         this.rowDbClick = function (e) {
-            self.trigger('rowDbClick', self.Item);
+            self.trigger('rowDbClick', self.rgSelectedRow);
             var obj = e.target;
             var cell = {
-                item: self.Item,
+                item: self.rgSelectedRow,
                 cell: obj
             }
             self.trigger('cellDbClick', cell);
         }
 
         this.btnClicked = function (e) {
-            var item = self.Item;
+            var item = self.rgSelectedRow;
             if (e.item.type == 'checkbox' || e.item.type == 'radio') {
                 item.checked = e.target.checked;
             }
@@ -349,7 +351,7 @@ riot.tag2('rg', '<div class="riotGrid"> <div class="riotGridHeader {topPanelCss}
                     self.gridData[i].rowNo = (self.selectedPage * self.listCount) + i + 1;
                 }
             }
-            self.Item = null;
+            self.rgSelectedRow = null;
             self.trigger('pageChanged', self.selectedPage + 1);
             if (self.cols.length == 0)
                 self.update();
